7211af87f8c67b2c20c8531e5ce0758e8da528c5
aaryxn-g <aaryangupta122@gmail.com>
2024-12-04 21:52:40 +0400

    Update AddSub.c

--- Arithmetic/AddSub.c
+++ Arithmetic/AddSub.c
@@ -4,8 +4,8 @@
 int main() {
     int a = 5, b = 10;
     int sum = a + b;
-    int product = a * b;
+    int diff = a - b;
     printf("Sum: %d\n", sum);
-    printf("Product: %d\n", product);
+    printf("Product: %d\n", diff);
     return 0;
 }
