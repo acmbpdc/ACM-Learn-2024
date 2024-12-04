2bce5080882acfe2f42ea2cabc8ccdc45e747822
aaryxn-g <aaryangupta122@gmail.com>
2024-12-04 21:45:07 +0400

    Create AddSub.c

---
+++ Arithmetic/AddSub.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10;
+    int sum = a + b;
+    int product = a * b;
+    printf("Sum: %d\n", sum);
+    printf("Product: %d\n", product);
+    return 0;
+}
