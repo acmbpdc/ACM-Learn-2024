d9c30803a88c2b9a2aa20746a9002065d8d59d03
aaryxn-g <aaryangupta122@gmail.com>
2024-12-05 09:03:47 +0400

    Update Question1.c

--- Practice/Question1.c
+++ Practice/Question1.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
-// Function to calculate factorial recursively
-int factorial(int n) {
-    if (n == 0 || n == 1) {
-        return 1;
+// Function to reverse a string using recursion
+void reverseString(char str[], int start, int end) {
+    // Base case: if the start index is greater or equal to end index
+    if (start >= end) {
+        return;
     }
-    return n * factorial(n - 1);
+    
+    // Swap characters at start and end
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    
+    // Recursive call
+    reverseString(str, start + 1, end - 1);
 }
 
 int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
     
-    printf("Factorial of %d is %d\n", num, factorial(num));
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    
+    reverseString(str, 0, length - 1);
+    printf("Reversed string: %s\n", str);
+    
     return 0;
 }
