5ee9271ab8292f0cb5d74653d30059953d2af82f
aaryxn-g <aaryangupta122@gmail.com>
2024-12-05 09:05:13 +0400

    Update Question4.c

--- Practice/Question4.c
+++ Practice/Question4.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-// Function to calculate Fibonacci number recursively
-int fibonacci(int n) {
-    if (n <= 1) {
-        return n;
+// Function to calculate sum of elements in an array using pointers
+int sumOfArray(int *arr, int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += *(arr + i); // Using pointer to access elements
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return sum;
 }
 
 int main() {
-    int n;
-    printf("Enter the position of Fibonacci sequence: ");
-    scanf("%d", &n);
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
     
-    printf("Fibonacci number at position %d is %d\n", n, fibonacci(n));
+    printf("Sum of elements in the array: %d\n", sumOfArray(arr, n));
+    
     return 0;
 }
