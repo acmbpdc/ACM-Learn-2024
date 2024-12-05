63a4d85208bc1e765568708393cde3d574b6b19c
aaryxn-g <aaryangupta122@gmail.com>
2024-12-05 08:55:43 +0400

    Create Question2.c

---
+++ Practice/Question2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function to reverse a string
+void reverseString(char str[]) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    
+    return 0;
+}
